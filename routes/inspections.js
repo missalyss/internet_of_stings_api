389111badc650a4ae78d778fd8f0966fb1a4e220
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-07-13 12:48:49 -0700

    passport middleware through all inspections routes. inspect create has decoding middleware

--- routes/inspections.js
+++ routes/inspections.js
@@ -5,6 +5,67 @@ const jwt = require('jwt-simple');
 const passport = require('passport')
 require('../db/passport')(passport)
 
+// INDEX LOGS
+router.get('/', passport.authenticate('jwt', {session: false}),
+function(req, res, next) {
+  var token = getToken(req.headers)
+  if (token) {
+    var decoded = jwt.decode(token, process.env.JWT_TOKEN)
+
+    knex('inspections').where('user_id', decoded.id).then(allLogs => {
+      res.json(allLogs)
+    })
+  } else {
+    return res.status(403).json({success: false, msg: 'No token provided.'})
+  }
+})
+
+// SHOW LOG
+router.get('/:id', passport.authenticate('jwt', {session: false}),
+function(req, res, next) {
+    const id = req.params.id
+
+    knex('inspections').where({id}).then(thisLog => {
+      res.json(thisLog)
+    })
+})
+
+// CREATE LOG
+router.post('/', passport.authenticate('jwt', {session: false}),
+function(req, res, next) {
+  var token = getToken(req.headers)
+  if (token) {
+    var decoded = jwt.decode(token, process.env.JWT_TOKEN)
+    req.body.user_id = decoded.id
+
+    knex('inspections').insert(req.body, '*').then(newLog => {
+      res.json(newLog)
+    })
+  } else {
+    return res.status(403).json({success: false, msg: 'No token provided.'})
+  }
+})
+
+// UPDATE LOG
+router.put('/:id', passport.authenticate('jwt', {session: false}),
+function(req, res, next) {
+    const id = req.params.id
+
+    knex('inspections').where({id}).update(req.body, '*').then(updatedLog => {
+      res.json(updatedLog)
+    })
+})
+
+// DESTROY LOG
+router.delete('/:id', passport.authenticate('jwt', {session: false}),
+function(req, res, next) {
+    const id = req.params.id
+
+    knex('inspections').where({id}).del().then(() => {
+      res.send()
+    })
+})
+
 getToken = function (headers) {
   if (headers && headers.authorization) {
     var parted = headers.authorization.split(' ')
@@ -18,54 +79,5 @@ getToken = function (headers) {
     return null
   }
 }
-
-// INDEX LOGS
-router.get('/', passport.authenticate('jwt', {session: false}),
-function(req, res, next) {
-  console.log('hello')
-  var token = getToken(req.headers)
-  console.log('token ', token)
-  if (token) {
-    var decoded = jwt.decode(token, process.env.JWT_TOKEN)
-    console.log('decoded ', decoded)
-    knex('inspections').where('user_id', decoded.id).then(allLogs => {
-      console.log('logs ', allLogs)
-      res.json(allLogs)
-    })
-  } else {
-    return res.status(403).json({success: false, msg: 'No token provided.'})
-  }
-})
-
-// SHOW LOG
-router.get('/:id', function(req, res, next) {
-  const id = req.params.id
-  knex('inspections').where({id}).then(thisLog => {
-    res.json(thisLog)
-  })
-})
-
-// CREATE LOG
-router.post('/', function(req, res, next) {
-  knex('inspections').insert(req.body, '*').then(newLog => {
-    res.json(newLog)
-  })
-})
-
-// UPDATE LOG
-router.put('/:id', function(req, res, next) {
-  const id = req.params.id
-  knex('inspections').where({id}).update(req.body, '*').then(updatedLog => {
-    res.json(updatedLog)
-  })
-})
-
-// DESTROY LOG
-router.delete('/:id', function(req, res, next) {
-  const id = req.params.id
-  knex('inspections').where({id}).del().then(() => {
-    res.send()
-  })
-})
 
 module.exports = router
