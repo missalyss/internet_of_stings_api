4d14c53dfd18ce985a700ee23b41b9cf0e448496
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-07-13 13:16:51 -0700

    get all must be authorized for humiture. commented out pointless routes, will delete later

--- routes/humiture.js
+++ routes/humiture.js
@@ -1,16 +1,27 @@
 const express = require('express')
 const router = express.Router()
 const knex = require('../db/connection')
+const jwt = require('jwt-simple');
+const passport = require('passport')
+require('../db/passport')(passport)
 
 // INDEX STATS
-router.get('/', function(req, res, next) {
-  knex('humiture').then(allStats => {
-    res.json(allStats)
-  })
+router.get('/', passport.authenticate('jwt', {session: false}), function(req, res, next) {
+  var token = getToken(req.headers)
+  if (token) {
+    var decoded = jwt.decode(token, process.env.JWT_TOKEN)
+
+    knex('humiture').where('user_id', decoded.id).then(allStats => {
+      res.json(allStats)
+    })
+  } else {
+    return res.status(403).json({success: false, msg: 'No token provided.'})
+  }
 })
 
+
 // SHOW STAT
-router.get('/:id', function(req, res, next) {
+router.get('/:id', passport.authenticate('jwt', {session: false}), function(req, res, next) {
   const id = req.params.id
   knex('humiture').where({id}).then(thisStat => {
     res.json(thisStat)
@@ -18,26 +29,40 @@ router.get('/:id', function(req, res, next) {
 })
 
 // CREATE STAT
-router.post('/', function(req, res, next) {
-  knex('humiture').insert(req.body, '*').then(newStat => {
-    res.json(newStat)
-  })
-})
+// router.post('/', passport.authenticate('jwt', {session: false}), function(req, res, next) {
+//   knex('humiture').insert(req.body, '*').then(newStat => {
+//     res.json(newStat)
+//   })
+// })
 
 // UPDATE STAT
-router.put('/:id', function(req, res, next) {
-  const id = req.params.id
-  knex('humiture').where({id}).update(req.body, '*').then(updatedStat => {
-    res.json(updatedStat)
-  })
-})
+// router.put('/:id', passport.authenticate('jwt', {session: false}), function(req, res, next) {
+//   const id = req.params.id
+//   knex('humiture').where({id}).update(req.body, '*').then(updatedStat => {
+//     res.json(updatedStat)
+//   })
+// })
 
 // DESTROY STAT
-router.delete('/:id', function(req, res, next) {
+router.delete('/:id', passport.authenticate('jwt', {session: false}), function(req, res, next) {
   const id = req.params.id
   knex('humiture').where({id}).del().then(() => {
     res.send()
   })
 })
+
+getToken = function (headers) {
+  if (headers && headers.authorization) {
+    var parted = headers.authorization.split(' ')
+    if (parted.length === 2) {
+      return parted[1]
+    } else {
+      return null
+    }
+    return headers.authorization
+  } else {
+    return null
+  }
+}
 
 module.exports = router
