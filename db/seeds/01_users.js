401c1c467ef2215a63f2500fc2b2b3932d40f8bf
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-07-24 20:17:34 -0700

    updated user seeds, deleted inspection seeds

--- db/seeds/01_users.js
+++ db/seeds/01_users.js
@@ -3,8 +3,8 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return knex('users').insert([
-        {id: 1, email: 'a@e.com', username: 'ae', hashed_password: '$2a$12$/tAYJHIfXmCgFrvTa39DYeAhitwbZ7lHAXEL13MoU6fHazLSDnnSG'},
-        {id: 2, email: 'a1@e.com', username: 'ae1', hashed_password: '$2a$12$/tAYJHIfXmCgFrvTa39DYeAhitwbZ7lHAXEL13MoU6fHazLSDnnSG'}
+        {id: 1, email: 'demo@demo.com', username: 'demo', hashed_password: '$2a$12$Y.3f3yY.0X9lscXZUCmZ..PAK7kG7Wf/8sg00AXMs.UiiZfJCptz2'},
+        {id: 2, email: 'missalyss@email.com', username: 'missalyss', hashed_password: '$2a$12$Y.3f3yY.0X9lscXZUCmZ..PAK7kG7Wf/8sg00AXMs.UiiZfJCptz2'}
       ])
     })
     .then(function () {
